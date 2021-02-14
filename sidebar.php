91b86200716ea0171ccded91c1badfd6099c6eab
Infvyr <allfortrashmail@gmail.com>
2021-02-14 12:31:47 +0200

    added sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package sleet
+ */
+
+?>
+<aside id="sidebar-1" class="widget-area" role="complementary">
+	<?php dynamic_sidebar('sidebar-1'); ?>
+</aside>
