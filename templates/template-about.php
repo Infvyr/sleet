958a4e72c31f3080a3c53bb8a4df472fae66a115
Infvyr <allfortrashmail@gmail.com>
2021-03-01 21:16:31 +0200

    added tabs

--- templates/template-about.php
+++ templates/template-about.php
@@ -1,4 +1,32 @@
 <?php
+
 /**
  * Template Name: About
  */
+
+get_header();
+
+?>
+
+<div class="tabs">
+
+    <ul class="tabs__list">
+        <li class="tabs__li">
+            <button class="tabs__btn">Tab 1</button>
+        </li>
+        <li class="tabs__li">
+            <button class="tabs__btn">Tab 2</button>
+        </li>
+        <li class="tabs__li">
+            <button class="tabs__btn">Tab 3</button>
+        </li>
+    </ul>
+
+    <div class="tabs__content">Tab content 1</div>
+    <div class="tabs__content">Tab content 2</div>
+    <div class="tabs__content">Tab content 3</div>
+</div>
+
+<?php
+
+get_footer();
