616dcedc7120dbe87748bbbb370f93fd8fefd07b
Infvyr <allfortrashmail@gmail.com>
2021-03-06 21:41:23 +0200

    updated domloaded event

--- assets/js/sleet-scripts.js
+++ assets/js/sleet-scripts.js
@@ -2,6 +2,10 @@ import MobileMenu from "./components/MobileMenu";
 import Tabs from "./components/Tabs";
 
 window.addEventListener("DOMContentLoaded", () => {
-    MobileMenu();
-    Tabs();
+    try {
+        MobileMenu();
+        Tabs();
+    } catch (error) {
+        console.log(error);
+    }
 });
