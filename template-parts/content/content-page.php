ef970c234803db10afb4e7ca055d420b7983749a
Infvyr <allfortrashmail@gmail.com>
2021-03-06 21:28:18 +0200

    edited footer layout

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying page content in page.php
  *
@@ -11,29 +12,29 @@
 
 <article <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="post__title">', '</h1>' ); ?>
+		<?php the_title('<h1 class="post__title">', '</h1>'); ?>
 	</header>
 
 	<div class="entry-content">
 		<?php the_content(); ?>
 	</div>
 
-	<?php if ( get_edit_post_link() ) : ?>
+	<?php if (get_edit_post_link()) : ?>
 
-		<footer class="entry-footer">
+		<footer class="post__footer">
 			<?php
 			edit_post_link(
 				sprintf(
 					wp_kses(
 						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'sleet' ),
+						__('Edit <span class="screen-reader-text">%s</span>', 'sleet'),
 						array(
 							'span' => array(
 								'class' => array(),
 							),
 						)
 					),
-					wp_kses_post( get_the_title() )
+					wp_kses_post(get_the_title())
 				),
 				'<span class="edit-link">',
 				'</span>'
